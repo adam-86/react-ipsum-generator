2f203cb34d35c781e042f801632cfde459b4334b
adam <adam@Adams-MacBook-Pro.local>
2019-03-30 14:30:42 -0700

    added media queries for small screens

--- src/components/options/Type.js
+++ src/components/options/Type.js
@@ -17,8 +17,10 @@ export default class Type extends Component {
     return (
       <div>
         <label>All meat or meat and filler?</label>
+        <br />
         <select onChange={this.onChange.bind(this)}>
           <option value="all-meat">all meat</option>
+
           <option value="meat-and-filler">meat and filler</option>
         </select>
       </div>
