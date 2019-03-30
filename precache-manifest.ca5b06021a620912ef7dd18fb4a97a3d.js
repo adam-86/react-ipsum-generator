8ebe4ae2bdd7ed20092e9796f920682f74977fb6
adam <adam@Adams-MacBook-Pro.local>
2019-03-30 13:25:27 -0700

    Updates

---
+++ precache-manifest.ca5b06021a620912ef7dd18fb4a97a3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a59bc820293bb3fa81d6b4bd49c1312",
+    "url": "/react-ipsum-generator/static/media/pig.3a59bc82.svg"
+  },
+  {
+    "revision": "8f431739b0c3197b1b4f",
+    "url": "/react-ipsum-generator/static/js/runtime~main.b17bcc71.js"
+  },
+  {
+    "revision": "802f9c21153c3d2c798b",
+    "url": "/react-ipsum-generator/static/js/main.5fbdbeaf.chunk.js"
+  },
+  {
+    "revision": "9aa3f2d41fbd0badb467",
+    "url": "/react-ipsum-generator/static/js/2.a437155b.chunk.js"
+  },
+  {
+    "revision": "802f9c21153c3d2c798b",
+    "url": "/react-ipsum-generator/static/css/main.e02b0b98.chunk.css"
+  },
+  {
+    "revision": "8fca4a1d0014ebdfb49ef47dfdf7305a",
+    "url": "/react-ipsum-generator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.278942cefdba32ba6996313a96b752d6.js"
+  "/react-ipsum-generator/precache-manifest.ca5b06021a620912ef7dd18fb4a97a3d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-ipsum-generator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
